34cde16b2f834b7787265afc448195b19059ae6d
Nandan Hegde <hegdenandan013@gmail.com>
2023-10-16 19:49:54 +0530

    Arrays update

---
+++ Basics/Array.js
@@ -0,0 +1,48 @@
+var friend1 = "arjun";
+var friend2 = "Visal";
+var friend3 = "Ramesh";
+
+var myFriends = [friend1,friend2,friend3];
+console.log(myFriends[1]);
+
+//for loop
+for( var i =0; i<myFriends.length;i++)
+{
+    console.log(myFriends[i]);
+}
+
+//for of loop
+for(let elements of myFriends)
+{
+    console.log(elements);
+}
+
+myFriends.forEach((elements,index,array)=>{
+console.log(elements+index);
+
+});
+
+
+const myNumbers = [1,2,3,4,5];
+myNumbers.unshift(4,6);
+console.log(myNumbers);
+
+const plants = ['brocoli','cauliflower','kale','tomato','cabbage'];
+console.log(plants.pop());
+console.log(plants);
+
+console.log(plants.shift());
+console.log(plants);
+
+//sol1
+const months = ['Jan','Feb','Mar','Apr','May'];
+const newMonth = months.splice(months.length,0,"December");
+//sol2
+console.log(newMonth);
+
+//sol3
+const updatMonth = months.splice(2,1,'March');
+console.log(months);
+
+
+
--- EcmaScript/updates.js
+++ EcmaScript/updates.js
@@ -3,3 +3,38 @@ console.log(myName);
 
 let name = "Nandan";
 console.log(name);
+
+function bioData()
+{
+    const myFirstName = "Nandan";
+    console.log(myFirstName);
+}
+//console.log(myFirstName);//will give error since const is a block scope.
+
+
+//Template literals
+var myCity = "Sirsi";
+console.log(`My Name is ${myName} and I am from ${myCity}`);
+
+//default argument
+function add(a,b=5)
+{
+    var answer=(a+b);
+    return answer;
+}
+console.log(add(5));
+
+const sum = function()
+    {
+         console.log("Hey there");
+    }
+
+sum();//bracket is must to call anonymous function
+
+//Arrow Function
+const sumArrow = (a) =>{
+    console.log("Hello "+a);
+}
+
+sumArrow("Nandan");
+
