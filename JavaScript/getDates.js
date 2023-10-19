330fd154139c3fba06aec84ca0c97e5b7cd02187
Nandan Hegde <hegdenandan013@gmail.com>
2023-10-19 23:38:40 +0530

    new commits

---
+++ JavaScript/getDates.js
@@ -0,0 +1,12 @@
+console.log(new Date().getFullYear());
+console.log(new Date().getMonth());
+const months = ["January","February","March","April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var d = new Date();
+console.log(months[d.getMonth()]);
+console.log(new Date().getDate());
+console.log(new Date().getDay());
+console.log(new Date().getMinutes());
+console.log(new Date().getHours());
+console.log(new Date().getSeconds());
+console.log(new Date().getMilliseconds());
+console.group(new)
---
+++ JavaScript/math.js
@@ -0,0 +1,23 @@
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.SQRT1_2);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
+
+//Math methods
+console.log(Math.round(10.5));
+console.log(Math.ceil(10.5));
+console.log(Math.floor(10.5));
+console.log(Math.trunc(10.5));
+console.log(Math.round(10.5));
+console.log(Math.pow(10.5));
+console.log(Math.sqrt(10.5));
+console.log(Math.abs(10.5));
+console.log(Math.sin(10.5));
+console.log(Math.cos(10.5));
+console.log(Math.max(10,20));
+console.log(Math.min(10,5));
+console.log(Math.floor(Math.random()*100));
