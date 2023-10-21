b754639cd39bc1e4aa2ca54d74d73b03b4db650c
Nandan Hegde <hegdenandan013@gmail.com>
2023-10-21 21:26:02 +0530

    new changes

---
+++ JavaScript/error.js
@@ -0,0 +1,39 @@
+//javascript actually create error object with name and message property
+//Six different values can be returned by the error name property
+/*
+1)EvalError
+2)RangeError
+3)ReferenceError
+4)SyntaxError
+5)TypeError
+6)URIError  
+
+try{
+
+    consolelog(Hello);
+}catch(e)
+{
+    console.log(e);
+}finally{
+    console.log("Exception Bypassed");
+}
+
+
+//throw statement
+var num = 15;
+try{
+    if(num>20)
+    {
+        throw "Number is too high"
+    }
+    else if(num<10)
+    {
+        throw "number is too low";
+    }
+    else{
+        console.log("Valid number : "+num);
+    }
+}catch(err)
+{
+    console.log(err);
+}
