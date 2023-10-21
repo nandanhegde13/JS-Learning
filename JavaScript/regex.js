11eb7d6bbba34f07bd263bca061325b605026bee
Nandan Hegde <hegdenandan013@gmail.com>
2023-10-21 16:23:45 +0530

    new changes

---
+++ JavaScript/regex.js
@@ -0,0 +1,4 @@
+let text = "Nandan Hegde Hosmane";
+console.log(text.search(/HEGDE/i));
+console.log(text.match(/[n]/ig));
+console.log("rED,RE, GREEN,Red,blue".match(/(red|green)/ig));
---
+++ JavaScript/typeConversion.js
@@ -0,0 +1,6 @@
+console.log(Number("3.14"));
+console.log(Number("John"));
+console.log(typeof Number.parseInt("25.6"));
+console.log(typeof Number.parseFloat("23.6"));
+console.log(typeof String(34));
+console.log(typeof (25).toString());
