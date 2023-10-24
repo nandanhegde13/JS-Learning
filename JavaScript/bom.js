eff55b391d13513d98920732bf2ed4ff1300444d
Nandan Hegde <hegdenandan013@gmail.com>
2023-10-24 16:46:01 +0530

    new changes

---
+++ JavaScript/bom.js
@@ -0,0 +1,27 @@
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+
+//window.open()
+//window.close()
+//window.moveTO()
+//window.resizeTo()
+
+console.log(screen.width);
+console.log(screen.height);
+console.log(screen.availWidth);
+console.log(screen.availHeight);
+console.log(screen);
+console.log(window.location);
+console.log(window.location.href);
+console.log(window.location.hostname);
+console.log(window.location.pathname);
+console.log(window.location.protocol);
+console.log(history.back());
+console.log(history.forward());
+console.log(navigator.cookieEnabled);
+console.log(navigator.appCodeName);
+console.log(navigator.product);
+console.log(navigator.language);
+console.log(navigator.platform);
+console.log(navigator.onLine);
+console.log(document.cookie="username=Nandan Hegde");
